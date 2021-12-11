ba845a9994cb606a7257ecbdb809fa6858140eb4
Christina Maria Weber <weberchri@cip.ifi.lmu.de>
2021-12-11 14:13:10 +0100

    Syntax

---
+++ Syntax.hs
@@ -0,0 +1,27 @@
+Program         ::= Def ";" RestProgram
+RestProgram     ::= e | Def ";" RestProgram
+Def             ::= Variable Restdef
+RestDef         ::= "=" Expr | Variable Restdef
+LocDefs         ::= LocDef RestLocDefs
+RestLocDefs     ::= e | ";" LocDefs
+LocDef          ::= Variable "=" Expr
+Expr            ::= "let" LocDefs "in" Expr
+                  | "if" Expr "then" Expr "else" Expr         
+                  | OrExpr
+OrExpr          ::= AndExpr RestOrExpr
+RestOrExpr      ::= e | "|" OrExpr
+AndExpr         ::= NotExpr RestAndExpr
+RestAndExpr     ::= e | "&" AndExpr
+NotExpr         ::= "not" CompareExpr | CompareExpr
+CompareExpr     ::= AddExpr RestCompareExpr
+RestCompareExpr ::= e | ("<" | "==") AddExpr
+AddExpr         ::= MultExpr ("-" MultExpr2 | "+" RestAddExpr | e)
+-- AddExpr         ::= MultExpr RestAddExpr
+RestAddExpr     ::= e | MultExpr2 RestAddExpr
+MultExpr        ::= NegExpr ("/" MultExpr2 | "*" RestMultExpr | e)
+MultExpr2       ::= AtomicExpr ("/" MultExpr2 | "*" RestMultExpr | e)
+RestMultExpr    ::= e | MultExpr2 RestMultExpr
+NegExpr         ::= "-" AtomicExpr | AtomicExpr
+AtomicExpr      ::= (Number | Boolean | "(" Expr ")" | Variable) RestAtomicExpr
+RestAtomicExpr  ::= e | AtomicExpr RestAtomicExpr
+Variable        ::= Name
