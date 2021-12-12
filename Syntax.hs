dc387e4eabdec0f2db26b7c25777d9935aa89795
Christina Maria Weber <weberchri@cip.ifi.lmu.de>
2021-12-12 19:12:41 +0100

    Wenn in program eine nichtleere Liste ankommt, wird Nothing returnt, restMult jetzt mit atomic statt multExpr2, Syntax angepasst, doppelte return Statements angepasst

--- Syntax.hs
+++ Syntax.hs
@@ -3,7 +3,7 @@ RestProgram     ::= e | Def ";" RestProgram
 Def             ::= Variable Restdef Expr
 RestDef         ::= "=" | Variable Restdef
 LocDefs         ::= LocDef RestLocDefs
-RestLocDefs     ::= e | ";" LocDef RestLocDef
+RestLocDefs     ::= e | ";" LocDef RestLocDefs
 LocDef          ::= Variable "=" Expr
 Expr            ::= "let" LocDefs "in" Expr
                   | "if" Expr "then" Expr "else" Expr         
@@ -12,13 +12,13 @@ OrExpr          ::= AndExpr ["|" OrExpr]
 AndExpr         ::= NotExpr ["&" AndExpr]
 NotExpr         ::= ["not"] CompareExpr
 CompareExpr     ::= AddExpr [("<" | "==") AddExpr]
-AddExpr         ::= MultExpr ("-" MultExpr2 | "+" RestAddExpr | e)
-RestAddExpr     ::= e | MultExpr2 RestAddExpr
-MultExpr        ::= NegExpr ("/" AtomicExpr | "*" RestMultExpr | e)
+AddExpr         ::= MultExpr ["-" MultExpr2 | "+" RestAddExpr]
+RestAddExpr     ::= MultExpr2 ["+" RestAddExpr]
+MultExpr        ::= NegExpr ["/" AtomicExpr | "*" RestMultExpr]
 -- MultExpr2 is used after an operator instead of MultExpr to forbid
 -- the input of negation directly after an operator without parentheses
-MultExpr2       ::= AtomicExpr ("/" AtomicExpr | "*" RestMultExpr | e)
-RestMultExpr    ::= e | MultExpr2 -- RestMultExpr
+MultExpr2       ::= AtomicExpr ["/" AtomicExpr | "*" RestMultExpr]
+RestMultExpr    ::= AtomicExpr ["*" RestMultExpr]
 NegExpr         ::= ["-"] AtomicExpr
 AtomicExpr      ::= (Number | Boolean | "(" Expr ")" | Variable) RestAtomicExpr
 RestAtomicExpr  ::= e | (Number | Boolean | "(" Expr ")" | Variable) RestAtomicExpr
