adb8ef64c7a1b726e17df8ce254ac30112d0c9fe
Christina Maria Weber <weberchri@cip.ifi.lmu.de>
2021-12-12 01:32:11 +0100

    Syntax angepasst an verkürzte Or/And Funktionen

--- Syntax.hs
+++ Syntax.hs
@@ -1,26 +1,25 @@
 Program         ::= Def ";" RestProgram
 RestProgram     ::= e | Def ";" RestProgram
-Def             ::= Variable Restdef
-RestDef         ::= "=" Expr | Variable Restdef
+Def             ::= Variable Restdef Expr
+RestDef         ::= "=" | Variable Restdef
 LocDefs         ::= LocDef RestLocDefs
-RestLocDefs     ::= e | ";" LocDefs
+RestLocDefs     ::= e | ";" LocDef RestLocDef
 LocDef          ::= Variable "=" Expr
 Expr            ::= "let" LocDefs "in" Expr
                   | "if" Expr "then" Expr "else" Expr         
                   | OrExpr
-OrExpr          ::= AndExpr RestOrExpr
-RestOrExpr      ::= e | "|" OrExpr
-AndExpr         ::= NotExpr RestAndExpr
-RestAndExpr     ::= e | "&" AndExpr
-NotExpr         ::= "not" CompareExpr | CompareExpr
-CompareExpr     ::= AddExpr RestCompareExpr
-RestCompareExpr ::= e | ("<" | "==") AddExpr
+OrExpr          ::= AndExpr ["|" OrExpr]
+AndExpr         ::= NotExpr ["&" AndExpr]
+NotExpr         ::= ["not"] CompareExpr
+CompareExpr     ::= AddExpr [("<" | "==") AddExpr]
 AddExpr         ::= MultExpr ("-" MultExpr2 | "+" RestAddExpr | e)
 RestAddExpr     ::= e | MultExpr2 RestAddExpr
 MultExpr        ::= NegExpr ("/" MultExpr2 | "*" RestMultExpr | e)
+-- MultExpr2 is used after an operator instead of MultExpr to forbid
+-- the input of negation directly after an operator without parentheses
 MultExpr2       ::= AtomicExpr ("/" MultExpr2 | "*" RestMultExpr | e)
 RestMultExpr    ::= e | MultExpr2 RestMultExpr
-NegExpr         ::= "-" AtomicExpr | AtomicExpr
+NegExpr         ::= ["-"] AtomicExpr
 AtomicExpr      ::= (Number | Boolean | "(" Expr ")" | Variable) RestAtomicExpr
-RestAtomicExpr  ::= e | AtomicExpr RestAtomicExpr
+RestAtomicExpr  ::= e | (Number | Boolean | "(" Expr ")" | Variable) RestAtomicExpr
 Variable        ::= Name
