4f9a7dd947e7e23f65ab2eee119e95f57b0f6bfe
Lara <moric@cip.ifi.lmu.de>
2021-12-16 19:54:20 +0100

    Tokenizer und TokenizerUndParser fertig

---
+++ TokenizerUndParser/TokenizerUndParser.hs
@@ -0,0 +1,340 @@
+data Token
+    = Number Integer
+    | Name String
+    | Boolean Bool
+    | Or
+    | And
+    | Not
+    | LessThan
+    | Is
+    | Minus
+    | Plus
+    | DivBy
+    | Times
+    | OpenPar
+    | ClosePar
+    | Let
+    | In
+    | If
+    | Then
+    | Else
+    | Semicolon
+    | Equals
+    deriving (Eq, Show)
+
+spaceyfier :: String -> String
+spaceyfier xs = do
+   case xs of
+       ';' : _          -> " ;" ++ spaceyfier (tail xs) -- leerzeichen danach?
+       '<' : _          -> " < " ++ spaceyfier (tail xs)
+       '=' : '=' : rest -> " == " ++ spaceyfier (tail rest)
+       '+' : _          -> " + " ++ spaceyfier (tail xs)
+       '-' : _          -> " - " ++ spaceyfier (tail xs)
+       '*' : _          -> " * " ++ spaceyfier (tail xs)
+       '/' : _          -> " / " ++ spaceyfier (tail xs)
+       []               -> []
+       _                -> head xs : spaceyfier (tail xs)
+
+example = spaceyfier "f x == 3<4;" -- passt 
+       
+wordyfier :: String -> [String]
+wordyfier xs = words $ spaceyfier xs
+
+example2 = wordyfier "f x == 3<4;"
+
+tokenizer :: [String] -> [Token]
+tokenizer ("|"     : xs) = Or            : tokenizer xs
+tokenizer ("&"     : xs) = And           : tokenizer xs
+tokenizer ("not"   : xs) = Not           : tokenizer xs
+tokenizer ("<"     : xs) = LessThan      : tokenizer xs
+tokenizer ("=="    : xs) = Is            : tokenizer xs
+tokenizer ("-"     : xs) = Minus         : tokenizer xs
+tokenizer ("+"     : xs) = Plus          : tokenizer xs
+tokenizer ("/"     : xs) = DivBy           : tokenizer xs
+tokenizer ("*"     : xs) = Times         : tokenizer xs
+tokenizer ("("     : xs) = OpenPar       : tokenizer xs
+tokenizer (")"     : xs) = ClosePar      : tokenizer xs
+tokenizer ("Let"   : xs) = Let           : tokenizer xs
+tokenizer ("In"    : xs) = In            : tokenizer xs
+tokenizer ("If"    : xs) = If            : tokenizer xs
+tokenizer ("Then"  : xs) = Then          : tokenizer xs
+tokenizer ("Else"  : xs) = Else          : tokenizer xs
+tokenizer (";"     : xs) = Semicolon     : tokenizer xs
+tokenizer ("="     : xs) = Equals        : tokenizer xs
+tokenizer ("True"  : xs) = Boolean True  : tokenizer xs
+tokenizer ("False" : xs) = Boolean False : tokenizer xs
+tokenizer []             = []
+tokenizer (x:xs)         = do
+    if checkNumber x then Number (read x) : tokenizer xs else Name x : tokenizer xs
+
+example4 = tokenizerFinal "f x = 3*4+25"
+
+checkInt :: Char -> Bool
+checkInt x = do
+    case x of
+        '0' -> True
+        '1' -> True
+        '2' -> True
+        '3' -> True
+        '4' -> True
+        '5' -> True
+        '6' -> True
+        '7' -> True
+        '8' -> True
+        '9' -> True
+        _   -> False
+
+checkNumber :: String -> Bool
+checkNumber (x:xs) = checkInt x && checkNumber xs
+checkNumber [] = True
+
+tokenizerFinal xs = tokenizer $ wordyfier xs
+
+example3 = tokenizerFinal "f x = 3<4;" -- passt 
+
+newtype Program = Program [Definition] deriving Show
+
+data Definition = Definition [Expression] Expression deriving Show
+
+newtype LocDefs = LocDefs [LocDef] deriving Show
+
+data LocDef     = LocDef Expression Expression deriving Show
+
+tokUndPar xs = program $ tokenizerFinal xs
+
+example5 = tokUndPar "foo a b = a*b+123; bar x y = True == False;"
+
+data Expression
+    = LetX      LocDefs Expression
+    | IfX       Expression Expression Expression
+    | OrX       Expression Expression
+    | AndX      Expression Expression
+    | NotX      Expression
+    | LessThanX Expression Expression
+    | IsX       Expression Expression
+    | Diff      Expression Expression
+    | Sum       Expression Expression
+    | Div       Expression Expression
+    | Mult      Expression Expression
+    | Neg       Expression
+    | Function  Expression Expression
+    | Val       Integer
+    | BoolVal   Bool
+    | Variable  String
+    deriving Show
+
+type Parser a = [Token] -> Maybe (a, [Token])
+
+program :: Parser Program
+program xs1 = do
+    (e, xs2)  <- def xs1
+    case xs2 of
+        Semicolon : xs3 -> do
+            (es, xs4) <- restProgram xs3
+            case xs4 of
+                [] -> return (Program (e:es), xs4)
+                _  -> Nothing
+        _               -> Nothing
+
+restProgram :: Parser [Definition]
+restProgram xs1 = do
+    case xs1 of
+        Name i : _ -> do
+            (e, xs2) <- def xs1
+            case xs2 of
+                Semicolon : xs3 -> do
+                    (es, xs4) <- restProgram xs3
+                    return (e:es, xs4)
+                _               -> Nothing
+        _          -> return ([], xs1)
+
+def :: Parser Definition
+def xs1 = do
+    (e, xs2)  <- variable xs1
+    (es, xs3) <- restDef xs2
+    (f, xs4)  <- expr xs3
+    return (Definition (e:es) f, xs4)
+
+restDef :: Parser [Expression]
+restDef (Name i : xs1) = do
+    (is, xs2) <- restDef xs1
+    return (Variable i:is, xs2)
+restDef (Equals : xs1) = return ([], xs1)
+restDef _              = Nothing
+
+locDefs :: Parser LocDefs
+locDefs xs1 = do
+    (e, xs2)  <- locDef xs1
+    (es, xs3) <- restLocDefs xs2
+    return (LocDefs (e:es), xs3)
+
+restLocDefs :: Parser [LocDef]
+restLocDefs (Semicolon : xs1) = do
+    (e, xs2)  <- locDef xs1
+    (es, xs3) <- restLocDefs xs2
+    return (e:es, xs3)
+restLocDefs xs                = return ([], xs)
+
+locDef :: Parser LocDef
+locDef xs1 = do
+    (e, xs2)  <- variable xs1
+    case xs2 of
+        Equals : xs3 -> do
+            (es, xs4) <- expr xs3
+            return (LocDef e es, xs4)
+        _            -> Nothing
+
+expr :: Parser Expression
+expr (Let : xs1) = do
+    (e, xs2)  <- locDefs xs1
+    case xs2 of
+        In : xs3 -> do
+            (es, xs4) <- expr xs3
+            return (LetX e es, xs4)
+        _        -> Nothing
+expr (If : xs1)  = do
+    (e1, xs2) <- expr xs1
+    case xs2 of
+        Then : xs3 -> do
+            (e2, xs4) <- expr xs3
+            case xs4 of
+                Else : xs5 -> do
+                    (es, xs6) <- expr xs5
+                    return (IfX e1 e2 es, xs6)
+                _          -> Nothing
+        _          -> Nothing
+expr xs          = orExpr xs
+
+orExpr :: Parser Expression
+orExpr xs1 = do
+    (e, xs2)  <- andExpr xs1
+    case xs2 of
+        Or : xs3 -> do
+            (es, xs4) <- orExpr xs3
+            return (OrX e es, xs4)
+        _        -> return (e, xs2)
+
+andExpr :: Parser Expression
+andExpr xs1 = do
+    (e, xs2)  <- notExpr xs1
+    case xs2 of
+        And : xs3 -> do
+            (es, xs4) <- andExpr xs3
+            return (AndX e es, xs4)
+        _         -> return (e, xs2)
+
+notExpr :: Parser Expression
+notExpr (Not : xs1) = do
+    (e, xs2)  <- compareExpr xs1
+    return (NotX e, xs2)
+notExpr xs          = compareExpr xs
+
+compareExpr :: Parser Expression
+compareExpr xs1 = do
+    (e, xs2)  <- addExpr xs1
+    case xs2 of
+        LessThan : xs3 -> do
+            (es, xs4) <- addExpr xs3
+            return (LessThanX e es, xs4)
+        Is : xs3       -> do
+            (es, xs4) <- addExpr xs3
+            return (IsX e es, xs4)
+        _              -> return (e, xs2)
+
+addExpr :: Parser Expression
+addExpr xs1 = do
+    (e, xs2)  <- multExpr xs1
+    case xs2 of 
+        Minus : xs3 -> do
+            (es, xs4) <- multExpr2 xs3 -- damit 5--2 nicht geht
+            return (Diff e es, xs4)
+        Plus :  _   -> do
+            (es, xs3) <- restAddExpr xs2
+            return (foldl Sum e es, xs3)
+        _           -> return (e, xs2)
+
+restAddExpr :: Parser [Expression]
+restAddExpr (Plus : xs1)  = do
+    (e, xs2)  <- multExpr2 xs1 -- damit 5+-2 nicht geht
+    (es, xs3) <- restAddExpr xs2
+    return (e:es, xs3)
+restAddExpr xs            = return ([], xs)
+
+multExpr :: Parser Expression
+multExpr xs1 = do
+    (e, xs2)  <- negExpr xs1
+    case xs2 of 
+        DivBy : xs3 -> do
+            (es, xs4) <- atomicExpr xs3 -- negExpr übersprungen, damit 5/-2 nicht geht
+            return (Div e es, xs4)
+        Times : _   -> do
+            (es, xs3) <- restMultExpr xs2
+            return (foldl Mult e es, xs3)
+        _           -> return (e, xs2)
+
+multExpr2 :: Parser Expression -- falls keine Negation möglich sein soll, zB (5--3) ist doof
+multExpr2 xs1 = do
+    (e, xs2)  <- atomicExpr xs1
+    case xs2 of 
+        DivBy : xs3 -> do
+            (es, xs4) <- atomicExpr xs3
+            return (Div e es, xs4)
+        Times : _   -> do
+            (es, xs3) <- restMultExpr xs2
+            return (foldl Mult e es, xs3)
+        _           -> return (e, xs2)
+
+restMultExpr :: Parser [Expression]
+restMultExpr (Times : xs1) = do
+    (e, xs2)  <- atomicExpr xs1 -- damit 5--2 nicht geht
+    (es, xs3) <- restMultExpr xs2
+    return (e:es, xs3)
+restMultExpr xs            = return ([], xs)
+
+negExpr :: Parser Expression
+negExpr (Minus : xs1) = do
+    (e, xs2)  <- atomicExpr xs1
+    return (Neg e, xs2)
+negExpr xs            = atomicExpr xs
+
+atomicExpr :: Parser Expression
+atomicExpr (Number i : xs1)  = do
+    (is, xs2) <- restAtomicExpr xs1
+    return (foldl Function (Val i) is, xs2)
+atomicExpr (Boolean i : xs1) = do
+    (is, xs2) <- restAtomicExpr xs1
+    return (foldl Function (BoolVal i) is, xs2)
+atomicExpr (OpenPar : xs1)   = do
+    (e, xs2)  <- expr xs1
+    case xs2 of
+        ClosePar : xs3 -> do
+            (es, xs4) <- restAtomicExpr xs3
+            return (foldl Function e es, xs4)
+        _              -> Nothing
+atomicExpr (Name i : xs1)    = do
+    (is, xs2) <- restAtomicExpr xs1
+    return (foldl Function (Variable i) is, xs2)
+atomicExpr _                 = Nothing
+
+restAtomicExpr :: Parser [Expression]
+restAtomicExpr (Number i : xs1)  = do
+    (is, xs2) <- restAtomicExpr xs1
+    return (Val i:is, xs2)
+restAtomicExpr (Boolean i : xs1) = do
+    (is, xs2) <- restAtomicExpr xs1
+    return (BoolVal i:is, xs2)
+restAtomicExpr (OpenPar : xs1)   = do
+    (e, xs2)  <- expr xs1
+    case xs2 of
+        ClosePar : xs3 -> do
+            (es, xs4) <- restAtomicExpr xs3
+            return (e:es, xs4)
+        _              -> Nothing
+restAtomicExpr (Name i : xs1)    = do
+    (is, xs2) <- restAtomicExpr xs1
+    return (Variable i:is, xs2)
+restAtomicExpr xs                = return ([], xs)
+
+variable :: Parser Expression
+variable (Name i : xs) = return (Variable i, xs)
+variable _             = Nothing
