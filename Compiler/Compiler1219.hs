31ca6cd9dc6618d78fba7a29f6a895b672d7b05f
Christina Maria Weber <weberchri@cip.ifi.lmu.de>
2022-01-04 19:49:54 +0100

    :-(

--- Compiler/Compiler1219.hs
+++ Compiler/Compiler1219.hs
@@ -5,9 +5,15 @@ data Instruction -- = Instruction
     | Pushparam Int
     | Makeapp
     | Slide Int
-    | Reduce
+    -- | Reduce
     | Return
     | Halt
+    | Unwind
+    | Call
+    | Return
+    | Pushpre Op -- Op definieren
+    | Update Arg
+    | Operator Op
     -- {
     --     address :: String        -> Int
     --     add2arg :: HeapCell      -> HeapCell
@@ -25,10 +31,7 @@ data State = State
     {
         pc     :: Int,
         code   :: [Instruction],
-        -- stack  :: [Instruction],     -- Stack?
-        -- heap   :: [HeapCell],        -- Heap?
-        -- global :: String -> HeapCell -- Global?
-        stack  :: Stack, -- [Int]
+        stack  :: Stack,
         heap   :: Heap,
         global :: Global
     }
@@ -41,16 +44,34 @@ data HeapCell
     = APP HeapCell HeapCell -- Konstruktor für Knoten
     | DEF String Int Int    -- DEF f N Code-Adr, f = Funktion, N = Stelligkeit
     | VAL Type Value        -- Blätter
+    | IND Int               -- HeapAdress
+    | PRE Op
 
 type Type  = Int    -- 1 = Bool, 0 = Zahl
 type Value = Int    -- 0 = False, 1 = True oder jede andere Zahl falls Type 0
 
+data Op
+    = If
+    | Or
+    | And
+    | LessThan
+    | Is
+    | Plus
+    | Minus
+    | Times
+    | DivBy
+    | Not
+    deriving (Eq, Show) -- Token?
 
 compileProgram :: [Definition] -> State
 -- compileProgram (x:xs) = compileDef x & compileProgram xs
 -- compileProgram [] = []
+compileProgram xs = 0 ([Reset, Pushfun main, Call, Halt] ++ cProg xs) [] [] []
+    where
+        cProg (x:xs) = compileDef x : cProg xs
+        cProg []     = []
 
-compileDef :: [Expression] Expression -> State
+compileDef :: [Expression] Expression -> [Instruction]
 compileDef (f:xs) y = 
     -- let n   = length xs
 -- code = code ++ L:ÜbDef(Expr, posifyer xs, n)
@@ -63,26 +84,26 @@ posifyer xs = pos xs 1
         pos [] _ = []
         pos (x:xs) akk = (x, akk) : pos xs (akk + 1)
 
-compileLocDefs :: [LocDef] -> State
+compileLocDefs :: [LocDef] -> [Instruction]
 
-compileLocDef :: Expression Expression -> State
+compileLocDef :: Expression Expression -> [Instruction]
 
-compileExpr :: Expression -> State
+compileExpr :: Expression -> [Instruction]
 
-compileOrExpr :: Expression -> State
+-- compileOrExpr :: Expression -> State
 
-compileAndExpr :: Expression -> State
+-- compileAndExpr :: Expression -> State
 
-compileNotExpr :: Expression -> State
+-- compileNotExpr :: Expression -> State
 
-compileCompareExpr :: Expression -> State
+-- compileCompareExpr :: Expression -> State
 
-compileAddExpr :: Expression -> State
+-- compileAddExpr :: Expression -> State
 
-compileMultExpr :: Expression -> State
+-- compileMultExpr :: Expression -> State
 
-compileNegExpr :: Expression -> State
+-- compileNegExpr :: Expression -> State
 
-compileAtomicExpr :: Expression -> State
+-- compileAtomicExpr :: Expression -> State
 
-compileVariable :: Expression -> State
+-- compileVariable :: Expression -> State
