56494989f2bdf7417b6a9038a949ea4f3f60df6d
Dani <ebensberger@cip.ifi.lmu.de>
2022-01-06 12:04:02 +0100

    updating funktionsdefinition

--- Compiler/Compiler1219.hs
+++ Compiler/Compiler1219.hs
@@ -22,6 +22,25 @@ data Instruction -- = Instruction
     -- }
     -- | ...
 
+data Expression
+    = LetX      LocDefs Expression
+    | IfX       Expression Expression Expression
+    | OrX       Expression Expression
+    | AndX      Expression Expression
+    | NotX      Expression
+    | LessThanX Expression Expression
+    | IsX       Expression Expression
+    | Diff      Expression Expression
+    | Sum       Expression Expression
+    | Div       Expression Expression
+    | Mult      Expression Expression
+    | Neg       Expression
+    | Function  Expression Expression
+    | Val       Integer
+    | BoolVal   Bool
+    | Variable  String
+    deriving Show
+
 -- data Stack = Stack []
 -- data Heap = APP HeapCell HeapCell
 -- data Global
@@ -41,7 +60,7 @@ type Heap   = [HeapCell]
 type Global = [(String, Int)]
 
 data HeapCell 
-    = APP HeapCell HeapCell -- Konstruktor für Knoten
+    = APP Int Int           -- Konstruktor für Knoten
     | DEF String Int Int    -- DEF f N Code-Adr, f = Funktion, N = Stelligkeit
     | VAL Type Value        -- Blätter
     | IND Int               -- HeapAdress
@@ -63,6 +82,12 @@ data Op
     | Not
     deriving (Eq, Show) -- Token?
 
+posifyer :: [Expression] -> [(Expression,Int)]
+posifyer xs = pos xs 1
+    where 
+        pos [] _ = []
+        pos (x:xs) akk = (x, akk) : pos xs (akk + 1)
+
 compileProgram :: [Definition] -> State
 -- compileProgram (x:xs) = compileDef x & compileProgram xs
 -- compileProgram [] = []
@@ -71,24 +96,35 @@ compileProgram xs = 0 ([Reset, Pushfun main, Call, Halt] ++ cProg xs) [] [] []
         cProg (x:xs) = compileDef x : cProg xs
         cProg []     = []
 
-compileDef :: [Expression] Expression -> [Instruction]
-compileDef (f:xs) y = 
-    -- let n   = length xs
+compileDef :: Definition -> [Instruction]
+compileDef (Definition (fun:args) body) = compileExpr body localEnv ++ [Slide 1, Reduce, Return]
+-- let n   = length xs
 -- code = code ++ L:ÜbDef(Expr, posifyer xs, n)
 -- heap = heap ++ DEF f n L
 -- global = global ++ (f, addr DEF f)
 
-posifyer :: [Expression] -> [(Expression,Int)]
-posifyer xs = pos xs 1
-    where 
-        pos [] _ = []
-        pos (x:xs) akk = (x, akk) : pos xs (akk + 1)
-
 compileLocDefs :: [LocDef] -> [Instruction]
 
-compileLocDef :: Expression Expression -> [Instruction]
+-- compileLocDef :: Expression -> Expression -> [Instruction]
 
-compileExpr :: Expression -> [Instruction]
+compileExpr :: Expression -> [(Expression, Int)] -> [Instruction]
+compileExpr (LetX      a b)   ((u,v):xs)) = ... compareExpr u xs
+compileExpr (IfX       a b c) env) = 
+compileExpr (OrX       a b)   env) = 
+compileExpr (AndX      a b)   env) = 
+compileExpr (NotX      a)     env) = 
+compileExpr (LessThanX a b)   env) = 
+compileExpr (IsX       a b)   env) = 
+compileExpr (Diff      a b)   env) = 
+compileExpr (Sum       a b)   env) = 
+compileExpr (Div       a b)   env) = 
+compileExpr (Mult      a b)   env) = 
+compileExpr (Neg       a)     env) = 
+compileExpr (Function  a b)   env) = Pushfun a
+compileExpr (Val       a)     env) = Pushval Num a
+compileExpr (BoolVal   a)     env) = Pushval Bool w -- w ist 0 oder 1  
+    -- if a == False then Pushval Bool 0 else Pushval Bool 1
+compileExpr (Variable  a)     env) = Pushparam (snd env)
 
 -- compileOrExpr :: Expression -> State
 
